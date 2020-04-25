a266a308d2c19f5b01460bd407b01a1180bd9d40
galin-kostadinov <gk_1988@abv.bg>
2020-04-25 21:53:55 +0300

    update index & home  -> added getAllPromotedProducts()

--- app/src/main/java/org/gkk/bioshopapp/web/controller/HomeController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/controller/HomeController.java
@@ -1,24 +1,46 @@
 package org.gkk.bioshopapp.web.controller;
 
+import org.gkk.bioshopapp.service.model.ProductPromoteServiceModel;
+import org.gkk.bioshopapp.service.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class HomeController extends BaseController {
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
 
     @GetMapping("/")
-    public ModelAndView index(){
-        return super.view("index");
+    public ModelAndView index(ModelAndView model) {
+        List<ProductPromoteServiceModel> products = this.productService.getAllPromotedProducts();
+
+        if (!products.isEmpty()){
+            model.addObject("promotedProducts", products);
+        }
+
+        return super.view("index", model);
     }
 
     @GetMapping("/home")
-    public ModelAndView home(){
-        return super.view("home");
+    public ModelAndView home(ModelAndView model) {
+        List<ProductPromoteServiceModel> products = this.productService.getAllPromotedProducts();
+
+        if (!products.isEmpty()){
+            model.addObject("promotedProducts", products);
+        }
+
+        return super.view("home", model);
     }
 
     @GetMapping("/contacts")
-    public ModelAndView contact(){
+    public ModelAndView contact() {
         return super.view("contacts");
     }
 }
