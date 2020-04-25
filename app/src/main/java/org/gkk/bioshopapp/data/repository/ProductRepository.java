2251a6d4b496d46bbf800a0eaafc2ff5c6cdf492
galin-kostadinov <gk_1988@abv.bg>
2020-04-25 21:50:49 +0300

    add query

--- app/src/main/java/org/gkk/bioshopapp/data/repository/ProductRepository.java
+++ app/src/main/java/org/gkk/bioshopapp/data/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import org.gkk.bioshopapp.data.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
-
+    List<Product> findAllByPromotionNotNull();
 }
