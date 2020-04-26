1aad9b5d186d04c3839c1bf912e7df8e1b0a073b
galin-kostadinov <gk_1988@abv.bg>
2020-04-26 23:44:07 +0300

    add /promotion-table /update-promotion/{id} /remove-promotion/{id}

--- app/src/main/java/org/gkk/bioshopapp/web/controller/ProductController.java
+++ app/src/main/java/org/gkk/bioshopapp/web/controller/ProductController.java
@@ -1,8 +1,10 @@
 package org.gkk.bioshopapp.web.controller;
 
 import org.gkk.bioshopapp.service.model.CreateProductServiceModel;
+import org.gkk.bioshopapp.service.model.ProductPromoteServiceModel;
 import org.gkk.bioshopapp.service.model.ProductServiceModel;
 import org.gkk.bioshopapp.service.service.ProductService;
+import org.gkk.bioshopapp.service.service.PromotionService;
 import org.gkk.bioshopapp.web.model.ProductCreateModel;
 import org.gkk.bioshopapp.web.model.ProductDetailsModel;
 import org.modelmapper.ModelMapper;
@@ -17,22 +19,22 @@ import java.util.List;
 @RequestMapping("/product")
 public class ProductController extends BaseController {
     private final ProductService productService;
+    private final PromotionService promotionService;
     private final ModelMapper modelMapper;
 
-    public ProductController(ProductService productService, ModelMapper modelMapper) {
+    public ProductController(ProductService productService, PromotionService promotionService, ModelMapper modelMapper) {
         this.productService = productService;
+        this.promotionService = promotionService;
         this.modelMapper = modelMapper;
     }
 
     @GetMapping({"/", ""})
     public ModelAndView getAllProducts(ModelAndView model) {
-        List<ProductServiceModel> products = this.productService.getAllProducts();
+        List<ProductPromoteServiceModel> products = this.productService.getAllPromotedProductModel();
 
         model.addObject("products", products);
 
-        model.setViewName("product/all-products");
-
-        return model;
+        return super.view("product/products", model);
     }
 
     @GetMapping("/create")
@@ -58,19 +60,19 @@ public class ProductController extends BaseController {
         }
     }
 
-    @GetMapping("/all-products-table")
+    @GetMapping("/product-table")
     public ModelAndView getAllProductsTable(ModelAndView model) {
         List<ProductServiceModel> products = this.productService.getAllProducts();
 
         model.addObject("products", products);
 
-        return super.view("product/all-products-table", model);
+        return super.view("product/product-table", model);
     }
 
     @GetMapping("/details/{id}")
     public ModelAndView detailsProduct(@PathVariable String id, ModelAndView model) throws Exception {
         ProductDetailsModel product =
-                this.modelMapper.map(this.productService.getProductById(id), ProductDetailsModel.class);
+                this.modelMapper.map(this.productService.getPromotedProductModelById(id), ProductDetailsModel.class);
 
 
         model.addObject("product", product);
@@ -111,24 +113,75 @@ public class ProductController extends BaseController {
     public ModelAndView deleteProductConfirm(@PathVariable String id, @ModelAttribute ProductCreateModel model) throws Exception {
         this.productService.deleteProduct(id);
 
-        return super.redirect("/product/all-products-table");
+        return super.redirect("/product/product-table");
     }
 
     @GetMapping("/promote/{id}")
     public ModelAndView getPromoteForm(@PathVariable String id, ModelAndView model) throws Exception {
+        ProductServiceModel productServiceModel = this.productService.getProductById(id);
+
+        if (productServiceModel.getPromotion() != null) {
+            return super.redirect("/product/product-table");
+        }
+
         ProductDetailsModel product =
-                this.modelMapper.map(this.productService.getProductById(id), ProductDetailsModel.class);
+                this.modelMapper.map(productServiceModel, ProductDetailsModel.class);
 
         model.addObject("product", product);
         model.addObject("productId", id);
 
-        return super.view("product/promote-product-form", model);
+        return super.view("product/promote-form", model);
     }
 
     @PostMapping("/promote/{id}")
     public ModelAndView promote(@PathVariable String id, Integer discount) throws Exception {
         this.productService.promote(id, discount);
 
-        return super.redirect("/product/all-products-table");
+        return super.redirect("/product/product-table");
+    }
+
+    @GetMapping("/promotion-table")
+    public ModelAndView getAllPromotions(ModelAndView model) {
+        List<ProductPromoteServiceModel> products = this.productService.getPromotedProducts();
+
+        model.addObject("products", products);
+
+        return super.view("product/promotion-table", model);
+    }
+
+    @GetMapping("/update-promotion/{id}")
+    public ModelAndView getUpdatePromotionForm(@PathVariable String id, ModelAndView model) throws Exception {
+        ProductPromoteServiceModel product =
+                this.modelMapper.map(this.productService.getPromotedProductModelById(id), ProductPromoteServiceModel.class);
+
+        model.addObject("product", product);
+        model.addObject("productId", id);
+
+        return super.view("product/promotion-update-form", model);
+    }
+
+    @PostMapping("/update-promotion/{id}")
+    public ModelAndView updatePromotion(@PathVariable String id, Integer discount) throws Exception {
+        this.promotionService.update(id, discount);
+
+        return super.redirect("/product/promotion-table");
+    }
+
+    @GetMapping("/remove-promotion/{id}")
+    public ModelAndView getRemovePromotionForm(@PathVariable String id, ModelAndView model) throws Exception {
+        ProductPromoteServiceModel product =
+                this.modelMapper.map(this.productService.getPromotedProductModelById(id), ProductPromoteServiceModel.class);
+
+        model.addObject("product", product);
+        model.addObject("productId", id);
+
+        return super.view("product/promotion-remove-form", model);
+    }
+
+    @PostMapping("/remove-promotion/{id}")
+    public ModelAndView removePromotion(@PathVariable String id) {
+        this.promotionService.remove(id);
+
+        return super.redirect("/product/promotion-table");
     }
 }
