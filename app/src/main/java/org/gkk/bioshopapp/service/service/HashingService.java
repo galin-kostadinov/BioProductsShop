b9cb9583ca82b4fd444e917085539cbe38c066a4
galin-kostadinov <gk_1988@abv.bg>
2020-05-03 22:42:37 +0300

    added isPasswordMatch()

--- app/src/main/java/org/gkk/bioshopapp/service/service/HashingService.java
+++ app/src/main/java/org/gkk/bioshopapp/service/service/HashingService.java
@@ -2,4 +2,6 @@ package org.gkk.bioshopapp.service.service;
 
 public interface HashingService {
     String hash(String str);
+
+    boolean isPasswordMatch(String inputPassword, String oldPassword);
 }
