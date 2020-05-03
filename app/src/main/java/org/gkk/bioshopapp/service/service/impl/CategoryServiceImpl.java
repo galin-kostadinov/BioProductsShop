4107f693bed595b66ffbe648ad26fcb79668adaa
galin-kostadinov <gk_1988@abv.bg>
2020-05-03 22:43:42 +0300

    updated getCategoryByName() -> orElseThrow

--- app/src/main/java/org/gkk/bioshopapp/service/service/impl/CategoryServiceImpl.java
+++ app/src/main/java/org/gkk/bioshopapp/service/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package org.gkk.bioshopapp.service.service.impl;
 import org.gkk.bioshopapp.data.model.Category;
 import org.gkk.bioshopapp.data.model.ProductType;
 import org.gkk.bioshopapp.data.repository.CategoryRepository;
+import org.gkk.bioshopapp.error.CategoryNotFoundException;
 import org.gkk.bioshopapp.service.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category getCategoryByName(ProductType name) {
-        return this.categoryRepository.findByName(name);
+        return this.categoryRepository.findByName(name)
+                .orElseThrow(() ->new CategoryNotFoundException("Category not found!"));
     }
 }
